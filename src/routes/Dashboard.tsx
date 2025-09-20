4570383d03069de8a08b03e315329bc81c6cb2c1
lmkaka <bidenclave@gmail.com>
2025-09-21 00:17:51 +0530

    Update Dashboard.tsx

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useMemo } from 'react'
+import { useEffect, useRef, useState, useMemo } from 'react'
 import { supabase } from '../supabaseClient'
 import { useAuth } from '../context/AuthProvider'
 import MatchCard from '../components/MatchCard'
