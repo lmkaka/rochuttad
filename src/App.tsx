171a92109f024bf0f4d7dec6235b1588cc0c5df2
lmkaka <bidenclave@gmail.com>
2025-09-30 22:38:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -83,6 +83,11 @@ export default function App() {
           path="/stream" 
           element={<StreamPage />} 
         />
+
+          <Route 
+          path="/streamaus" 
+          element={<StreamPage />} 
+        />
         
         {/* Default Route */}
         <Route 
